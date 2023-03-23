75d23c0cfd7578f10ac3113fa5508f855c2068f2
Rashmiranjan117 <107473816+Rashmiranjan117@users.noreply.github.com>
2023-03-23 17:50:51 +0530

    added styles to login and sigup page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
-import "./App.css";
+import "./sass/app.css";
 import AllRoutes from "./Routes/AllRoutes";
 import Navbar from "./Components/Navbar";
 
